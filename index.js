61a87a52b2746f605b7e329aaa56c228e43b7909
Josh Robbs <josh@rustbeltwebworks.com>
2018-05-14 17:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function happyHolidayTo(string1,string2) {
 }
 function holidayCountdown(string1,string2) {
   
-  `It's ${string2} days until ${string1}!`
+  `It\'s ${string2} days until ${string1}!`
 }
